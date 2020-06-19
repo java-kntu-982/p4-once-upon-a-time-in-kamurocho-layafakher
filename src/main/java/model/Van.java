27c2a90ebf8263b11628db95ff66e242020c9872
layafakher <laya.fakher2000@gmail.com>
2020-06-19 09:29:22 +0430

    7

--- src/main/java/model/Van.java
+++ src/main/java/model/Van.java
@@ -1,7 +1,59 @@
 package model;
 
+import javafx.scene.control.Label;
+
 public class Van extends Studding {
     public Van(double durability) {
         super(durability);
+    }
+
+    @Override
+    public int getLevel() {
+        return super.getLevel();
+    }
+
+    @Override
+    public void setLevel(int level) {
+        super.setLevel(level);
+    }
+
+    @Override
+    public Label getLabel() {
+        return super.getLabel();
+    }
+
+    @Override
+    public void setLabel(Label label) {
+        super.setLabel(label);
+    }
+
+    @Override
+    public double getDurability() {
+        return super.getDurability();
+    }
+
+    @Override
+    public void setDurability(double durability) {
+        super.setDurability(durability);
+    }
+
+    @Override
+    public double getX() {
+        return super.getX();
+    }
+
+    @Override
+    public void setX(double x) {
+        super.setX(x);
+    }
+
+    @Override
+    public double getY() {
+        return super.getY();
+    }
+
+    @Override
+    public void setY(double y) {
+        super.setY(y);
     }
 }
